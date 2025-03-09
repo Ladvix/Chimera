daffb1d331b60ffe2471a53b2d2fd7b506db46ac
Ladvix <ladvix.official@gmail.com>
2025-03-09 20:27:35 +0300

    Update install.py

--- userbot/commands/install/install.py
+++ userbot/commands/install/install.py
@@ -5,7 +5,7 @@ from userbot import module
 
 def format_url(url):
     url = url.endswith('.git') == True and url[:-4] or url
-    new_url = f'{url}/archive/refs/heads/main.zip'
+    new_url = url + '/archive/refs/heads/main.zip'
 
     return new_url
 
@@ -15,12 +15,13 @@ def init(bot, client, message):
     url = format_url(data[1])
     module_name = data[3]
 
-    with open(f'{dirs.COMMANDS_PATH}install/templates/loading.html', encoding='utf-8') as f:
+    with open(dirs.COMMANDS_PATH + 'install/templates/loading.html', encoding='utf-8') as f:
         message.edit(f.read())
 
     try:
         git.clone(url, dirs.MODULES_PATH + module_name)
         module.launch(bot, module_name)
+        
         with open(dirs.COMMANDS_PATH + 'install/templates/success.html', encoding='utf-8') as f:
             s = Template(f.read())
             message.edit(s.substitute(module_name=module_name))
