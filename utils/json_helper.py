4b4e39a5f21bdeccc70a8213a77827235884ece7
Ladvix <ladvix.official@gmail.com>
2025-03-15 19:51:24 +0300

    Update json_helper.py

--- utils/json_helper.py
+++ utils/json_helper.py
@@ -4,9 +4,9 @@ import json
 
 def read(path):
     with open(path, 'r', encoding='utf-8') as f:
-        return json.loads(f.read())
+        return json.loads(f.read(), ensure_ascii=False)
 
 
 def write(path, data):
     with open(path, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(data))
+        f.write(json.dumps(data), ensure_ascii=False)
